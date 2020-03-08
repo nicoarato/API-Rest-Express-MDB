55a7e37b747df6a3a1c9844d73af5dc5a1536fb4
Nico Arato <nico.arato@gmail.com>
2020-03-08 00:31:50 -0300

    HTTP get

--- express-01/app.js
+++ express-01/app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 
+const usuarios = [
+    { id: 1, nombre: 'Luis' }, { id: 2, nombre: 'Maria' }, { id: 3, nombre: 'Ana' }
+];
+
 app.get('/', (req, res) => {
     res.send('Hola mundo desde Express.');
 });
 
 app.get('/api/usuarios', (req, res) => {
-    res.send(['Luis', 'Maria', 'Franco']);
+    res.send(usuarios);
 });
+
+app.get('/api/usuarios/:id', (req, res) => {
+
+    let usuario = usuarios.find(u => u.id === parseInt(req.params.id))
+
+    if (!usuario) {
+        res.status(404).send('El usuario no fue encontrado.');
+    } else {
+        res.send(usuario)
+    }
+});
+
+
+
 
 const port = process.env.PORT || 3000;
 
